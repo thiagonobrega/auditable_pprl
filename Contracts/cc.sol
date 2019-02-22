ab1ed8be7302cb0c4a88ddeb2de0850cfcee84a8
Thiago Nóbrega <thiagonobrega@uepb.edu.br>
2019-02-22 14:50:20 -0300

    BitsOPeration and working bf jaccard

--- Contracts/cc.sol
+++ Contracts/cc.sol
@@ -2,17 +2,29 @@ pragma solidity >0.4.99 <0.6.0;
 
 contract CC1 {
 
-//     function someOldFunction(uint8 a) external;
-//     function anotherOldFunction() external returns (bool);
-    
-//     function doSomething(OldContract a) public returns (bool) {
-//       a.someOldFunction(0x42);
-//       return a.anotherOldFunction();
-//    }
-
-    function compareBloom(bytes1 bf1, bytes1 bf2) public pure returns (bytes1) {
+    function compareBloom(bytes1 bf1, bytes1 bf2) public payable returns (bytes1) {
         return bf1 & bf2;
     }
 
+    function jaccardBloom(bytes1 bf1, bytes1 bf2,uint precision) public payable returns (uint256) {
+        // uint a = ;
+        // b = uint(bf1 | bf2);
+        uint inter = countBits(uint8(bf1 & bf2));
+        uint union = countBits(uint8(bf1 | bf2));
+        return inter*(10**precision)/union;
+    }
+
+    function countBits(uint num) public payable returns (uint) {
+        uint count = 0;
+
+        while (num != 0){
+            num = num & (num-1);
+            count = count + 1;
+        }
+
+        return count;
+    }
+    
+    // return inter/union
 
 }
