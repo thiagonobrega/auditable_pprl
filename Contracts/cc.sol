5dc848feaf5f221a3632ca646e16e7e1d894510d
Thiago Nóbrega <thiagonobrega@gmail.com>
2019-02-24 16:40:25 -0300

    Update cc.sol
    Expande de blomfilter size to 256bits and documentation

--- Contracts/cc.sol
+++ Contracts/cc.sol
@@ -1,21 +1,26 @@
 pragma solidity >0.4.99 <0.6.0;
 
-contract CC1 {
+contract ComparasionClassification {
 
-    function compareBloom(bytes1 bf1, bytes1 bf2) public payable returns (bytes1) {
-        return bf1 & bf2;
-    }
-
-    function jaccardBloom(bytes1 bf1, bytes1 bf2,uint precision) public payable returns (uint256) {
-        // uint a = ;
-        // b = uint(bf1 | bf2);
-        uint inter = countBits(uint8(bf1 & bf2));
-        uint union = countBits(uint8(bf1 | bf2));
+    /**
+    * Jaccard distance over to BloomFiltes
+    * @param bf1 
+    * @param bf2 
+    * 
+    * @return distance * 10**precison
+     */
+    function jaccardBloom(bytes32 bf1, bytes32 bf2,uint precision) public payable returns (uint256) {
+        uint inter = countBits(uint256(bf1 & bf2));
+        uint union = countBits(uint256(bf1 | bf2));
         return inter*(10**precision)/union;
     }
 
-    function countBits(uint num) public payable returns (uint) {
+    /**
+     * Count the number of bits in one uint256 bits
+     */
+    function countBits(uint256 callnum) public payable returns (uint) {
         uint count = 0;
+        uint256 num = callnum; //fixe later
 
         while (num != 0){
             num = num & (num-1);
@@ -24,4 +29,5 @@ contract CC1 {
 
         return count;
     }
+
 }
